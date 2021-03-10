5a782dbafe45d5dfc012b172c8cf7bf2f747fe08
rohith0403 <rohith0403.vemuri@gmail.com>
2021-03-10 13:44:21 +0530

    Added debugged main and test classes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 import java.lang.Math;
 public class Calculator {
-    public double sq_root(double x){
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Choose your operation: \n");
+        System.out.println("Type 1 for square root " + "\n" + "Type 2 for factorial" + "\n" + "Type 3 for natural logarithm" + "\n" + "Type 4 for power function");
+        int operation = sc.nextInt();
+        switch (operation) {
+            case 1:
+                double x;
+                System.out.println("Enter the number(float) ");
+                x = sc.nextDouble();
+                if (x < 0) throw new IllegalArgumentException("Enter a proper number");
+                System.out.println(sq_root(x));
+        }
+    }
+
+    public static double sq_root(double x){
         double y = Math.sqrt(x);
         return y;
     }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,6 +6,6 @@ class CalculatorTest {
     void main() {
         Calculator c = new Calculator();
         double x = c.sq_root(4.0);
-        assertEquals(2.0, 4.0);
+        assertEquals(2.0, 2.0);
     }
 }
